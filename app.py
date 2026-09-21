6e60acaec81e75e6208479988f13fd41dd8d2926
rodrigopeixoto-png <rodrigo.peixoto@sesp.pr.gov.br>
2026-09-21 11:26:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -332,7 +332,7 @@ with col_esq:
                 
             page_idx = pagina_selecionada - 1
             
-          with st.expander("👁️ Pré-visualizar Página Selecionada", expanded=True):
+            with st.expander("👁️ Pré-visualizar Página Selecionada", expanded=True):
                 st.markdown(f"**Página atual:** {pagina_selecionada}")
                 pix_preview = doc.load_page(page_idx).get_pixmap(dpi=72)
                 st.image(PILImage.open(io.BytesIO(pix_preview.tobytes("png"))), use_container_width=True)
@@ -402,7 +402,7 @@ with col_esq:
             with col_pag_c:
                 pag_croqui = st.number_input(f"Página do Croqui (1 a {total_paginas}):", min_value=1, max_value=total_paginas, value=1, key="num_croqui")
             
-           with st.expander("👁️ Pré-visualizar Croqui", expanded=True):
+            with st.expander("👁️ Pré-visualizar Croqui", expanded=True):
                 st.markdown(f"**Página atual:** {pag_croqui}")
                 pix_croqui = doc.load_page(pag_croqui - 1).get_pixmap(dpi=72)
                 st.image(PILImage.open(io.BytesIO(pix_croqui.tobytes("png"))), use_container_width=True)
